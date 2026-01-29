fed9aa92501cc86c645896f079896621d3dcf483
David Lougheed <david.lougheed@gmail.com>
2026-01-29 17:10:19 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,99 @@
+use noodles_fastq as fastq;
+use serde::Serialize;
+use std::fs::File;
+use std::io::{BufReader, Error};
+
+struct FastqStatsAgg {
+    read_lengths: Vec<usize>,
+    total_bases: usize,
+    total_gc: usize,
+}
+
+#[derive(Serialize)]
+struct FastqStatsReport {
+    n: usize,
+    n50: usize,
+    median_length: f64,
+    mean_length: f64,
+    total_bases: usize,
+    gc_prop: f64,
+    longest_read: usize,
+    shortest_read: usize,
+}
+
+fn n50(x: &Vec<usize>, total_bases: usize) -> usize {
+    let half_total_bases = total_bases / 2;
+    // x: already sorted smallest-to-largest; start at the largest end
+    let mut sum: usize = 0;
+    for i in(0..x.len()).rev() {
+        sum += x[i];
+        if sum >= half_total_bases {
+            return i;
+        }
+    }
+    0  // unreachable
+}
+
+fn median(x: &Vec<usize>) -> f64 {
+    if x.len() % 2 == 1 {
+        x[x.len() / 2] as f64
+    } else {
+        ((x[x.len() / 2] + x[(x.len() / 2) + 1]) as f64) / 2.0f64
+    }
+}
+
+fn fastq_report() -> Result<FastqStatsReport, Error> {
+    let mut stats_agg = FastqStatsAgg {
+        read_lengths: Vec::new(),
+        total_bases: 0,
+        total_gc: 0,
+    };
+
+    let mut reader: fastq::io::Reader<BufReader<File>> = File::open("bc1015.fq").map(BufReader::new).map(fastq::io::Reader::new)?;
+    let mut record = fastq::Record::default();
+
+    loop {
+        let bytes_read: usize = reader.read_record(&mut record)?;
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let seq = record.sequence();
+        let seq_len = seq.len();
+
+        stats_agg.read_lengths.push(seq_len);
+        stats_agg.total_bases += seq_len;
+        stats_agg.total_gc += seq.iter().filter(|&&b| b == b'G' || b == b'g' || b == b'C' || b == b'c').count();
+
+        // TODO
+    }
+
+    stats_agg.read_lengths.sort_unstable();
+
+    // ------------------------------------------------------------------------
+
+    let n_reads = stats_agg.read_lengths.len();
+    let total_bases = stats_agg.total_bases;
+
+    // TODO: histogram
+
+    Ok(FastqStatsReport {
+        n: n_reads,
+        n50: n50(&stats_agg.read_lengths, total_bases),
+        median_length: median(&stats_agg.read_lengths),
+        mean_length: total_bases as f64 / n_reads as f64,
+        total_bases,
+        gc_prop: stats_agg.total_gc as f64 / total_bases as f64,
+        longest_read: stats_agg.read_lengths[n_reads - 1],
+        shortest_read: stats_agg.read_lengths[0],
+    })
+}
+
+fn main() -> Result<(), Error> {
+    let report = fastq_report()?;
+
+    println!("{}", serde_json::to_string_pretty(&report)?);
+
+    Ok(())
+}
